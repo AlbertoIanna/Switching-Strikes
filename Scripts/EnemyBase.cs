4493c270fb2c2c76e6328fffbbe4195e63e60948
MI_C_01\Ianna A <ianna.alberto00@gmail.com>
2018-04-19 12:24:07 +0200

    Enemy and player scripts

---
+++ Scripts/EnemyBase.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour, IEnemy {
+
+    float speed;
+
+    public void DealDamage()
+    {
+        
+    }
+
+    public void EnemyMovement()
+    {
+        throw new System.NotImplementedException();
+    }
+
+  
+}
---
+++ Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    float timer;
+
+    private void FixedUpdate()
+    {
+        Movement();
+        if (timer >= 2.5)
+            CenterPlayer();
+    }
+
+    void Movement()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            transform.position += new Vector3(-3, 0, 0);
+            Debug.Log("sinistra");
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            transform.position += new Vector3(3, 0, 0);
+            Debug.Log("destra");
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            transform.position += new Vector3(0, 0, 3);
+            Debug.Log("su!");
+            }
+        if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            transform.position += new Vector3(0, 0, -3);
+            Debug.Log("Giù!");
+        }
+        timer += Time.deltaTime;
+    }
+
+    void CenterPlayer()
+    {
+        if (transform.position.x == 3 || transform.position.x ==-3)
+            transform.position = new Vector3(0,0,0);
+        if (transform.position.z == 3 || transform.position.z == -3)
+            transform.position = new Vector3(0, 0, 0);
+
+    }
+
+  
+}
