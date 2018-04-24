d8357ab48e71fb585415df5092bc98102f4c87c1
MI_C_01\Ianna A <ianna.alberto00@gmail.com>
2018-04-24 11:31:09 +0200

    Scene Charged

--- Assets/Scripts/EnemyBase.cs
+++ Assets/Scripts/EnemyBase.cs
@@ -2,12 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public  abstract class EnemyBase : MonoBehaviour, IEnemy {
+public class EnemyBase : MonoBehaviour{
 
+    bool GotoRight ;
+    bool GoDown ;
     float speed;
+    private Color EnemyColor;
+    private Vector3 respawnposition;
+    private Vector3 Arriveposition = new Vector3(0,0,0);
 
-    public abstract void DealDamage();
-    public abstract void EnemyMovement();
+    public  void DealDamage() { }
+    private void Start()
+    {
+        transform.position = respawnposition;
+    }
 
-  
+    private void Update()
+    {
+        Movement();
+    }
+
+    public void Movement()
+    {
+        if (transform.position.x > 0)
+            GotoRight = false;
+        if (transform.position.z < 0)
+            GoDown = false;
+
+        if (GotoRight == true)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime);
+            if (transform.position.x <= Arriveposition.x)
+                transform.position = respawnposition;
+        }
+        else
+        {
+            transform.Translate(Vector3.left * Time.deltaTime);
+            if (transform.position.x >= Arriveposition.x)
+                transform.position = respawnposition;
+        }
+
+        if (GoDown == true)
+        {
+            transform.Translate(Vector3.down* Time.deltaTime);
+            if (transform.position.z <= Arriveposition.z)
+                transform.position = respawnposition;
+        }
+        else
+        {
+            transform.Translate(Vector3.up* Time.deltaTime);
+            if (transform.position.x >= Arriveposition.z)
+                transform.position = respawnposition;
+        }
+    }
+
+
 }
